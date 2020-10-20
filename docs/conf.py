e38bbbdeb265b3bf4f8efe4f48afe901c17077af
tsutsu3 <tsutsu3prog@gmail.com>
2020-10-20 18:48:11 +0900

    Fix sphinx warnings

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,8 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+nitpick_ignore = [("py:class", "Match")]
+
 
 # -- Options for HTML output -------------------------------------------------
 
