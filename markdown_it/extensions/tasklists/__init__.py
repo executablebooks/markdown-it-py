cd433e69e96bf8b693a72c5f63560582bad58cd7
Wolmar Nyberg Åkerström <wolmar.n.akerstrom@nbis.se>
2020-11-12 00:17:30 +0100

    Processed by pre-commit + manually broken line for compliance

--- markdown_it/extensions/tasklists/__init__.py
+++ markdown_it/extensions/tasklists/__init__.py
@@ -102,7 +102,10 @@ def tasklists_plugin(
         checkbox = token_constructor("html_inline", "", 0)
         disabled_attr = 'disabled="disabled"' if disable_checkboxes else ""
         if token.content.startswith("[ ] "):
-            checkbox.content = f'<input class="task-list-item-checkbox" {disabled_attr} type="checkbox">'
+            checkbox.content = (
+                '<input class="task-list-item-checkbox" '
+                f'{disabled_attr} type="checkbox">'
+            )
         elif token.content.startswith("[x] ") or token.content.startswith("[X] "):
             checkbox.content = (
                 '<input class="task-list-item-checkbox" checked="checked" '
