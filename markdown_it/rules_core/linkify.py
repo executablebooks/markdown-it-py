4a97b09430e484b0898a074e7d838a0f0ab8af49
tsutsu3 <tsutsu3prog@gmail.com>
2020-11-18 20:09:10 +0900

    Fix bug caused by a misalignment of index 'i'

--- markdown_it/rules_core/linkify.py
+++ markdown_it/rules_core/linkify.py
@@ -44,7 +44,12 @@ def linkify(state: StateCore):
         # We scan from the end, to keep position when new tags added.
         # Use reversed logic in links start/end match
         assert tokens is not None
-        for i in range(len(tokens))[::-1]:
+        i = len(tokens)
+        while i >= 0:
+            i -= 1
+            if i < 0:
+                break
+            assert isinstance(tokens, list)
             currentToken = tokens[i]
 
             # Skip content of markdown links
@@ -57,74 +62,82 @@ def linkify(state: StateCore):
                     i -= 1
                 continue
 
-        # Skip content of html tag links
-        if currentToken.type == "html_inline":
-            if isLinkOpen(currentToken.content) and htmlLinkLevel > 0:
-                htmlLinkLevel -= 1
-            if isLinkClose(currentToken.content):
-                htmlLinkLevel += 1
-        if htmlLinkLevel > 0:
-            continue
+            # Skip content of html tag links
+            if currentToken.type == "html_inline":
+                if isLinkOpen(currentToken.content) and htmlLinkLevel > 0:
+                    htmlLinkLevel -= 1
+                if isLinkClose(currentToken.content):
+                    htmlLinkLevel += 1
+            if htmlLinkLevel > 0:
+                continue
 
-        if currentToken.type == "text" and state.md.linkify.test(currentToken.content):
-            text = currentToken.content
-            links = state.md.linkify.match(text)
+            if currentToken.type == "text" and state.md.linkify.test(
+                currentToken.content
+            ):
+                text = currentToken.content
+                links = state.md.linkify.match(text)
 
-            # Now split string to nodes
-            nodes = []
-            level = currentToken.level
-            lastPos = 0
+                # Now split string to nodes
+                nodes = []
+                level = currentToken.level
+                lastPos = 0
 
-            for ln in range(len(links)):
-                url = links[ln].url
-                fullUrl = normalizeLink(url)
-                if not validateLink(fullUrl):
-                    continue
+                for ln in range(len(links)):
+                    url = links[ln].url
+                    fullUrl = normalizeLink(url)
+                    if not validateLink(fullUrl):
+                        continue
 
-                urlText = links[ln].text
+                    urlText = links[ln].text
 
-                # Linkifier might send raw hostnames like "example.com", where url
-                # starts with domain name. So we prepend http:// in those cases,
-                # and remove it afterwards.
-                if not links[ln].schema:
-                    urlText = HTTP_RE.sub("", normalizeLinkText("http://" + urlText))
-                elif links[ln].schema == "mailto:" and TEST_MAILTO_RE.search(urlText):
-                    urlText = MAILTO_RE.sub("", normalizeLinkText("mailto:" + urlText))
-                else:
-                    urlText = normalizeLinkText(urlText)
+                    # Linkifier might send raw hostnames like "example.com", where url
+                    # starts with domain name. So we prepend http:// in those cases,
+                    # and remove it afterwards.
+                    if not links[ln].schema:
+                        urlText = HTTP_RE.sub(
+                            "", normalizeLinkText("http://" + urlText)
+                        )
+                    elif links[ln].schema == "mailto:" and TEST_MAILTO_RE.search(
+                        urlText
+                    ):
+                        urlText = MAILTO_RE.sub(
+                            "", normalizeLinkText("mailto:" + urlText)
+                        )
+                    else:
+                        urlText = normalizeLinkText(urlText)
 
-                pos = links[ln].index
+                    pos = links[ln].index
 
-                if pos > lastPos:
+                    if pos > lastPos:
+                        token = Token("text", "", 0)
+                        token.content = text[lastPos:pos]
+                        token.level = level
+                        nodes.append(token)
+
+                    token = Token("link_open", "a", 1)
+                    token.attrs = [["href", fullUrl]]
+                    token.level = level + 1
+                    token.markup = "linkify"
+                    token.info = "auto"
+                    nodes.append(token)
+
                     token = Token("text", "", 0)
-                    token.content = text[lastPos:pos]
+                    token.content = urlText
                     token.level = level
                     nodes.append(token)
 
-                token = Token("link_open", "a", 1)
-                token.attrs = [["href", fullUrl]]
-                token.level = level + 1
-                token.markup = "linkify"
-                token.info = "auto"
-                nodes.append(token)
+                    token = Token("link_close", "a", -1)
+                    token.level = level - 1
+                    token.markup = "linkify"
+                    token.info = "auto"
+                    nodes.append(token)
 
-                token = Token("text", "", 0)
-                token.content = urlText
-                token.level = level
-                nodes.append(token)
+                    lastPos = links[ln].last_index
 
-                token = Token("link_close", "a", -1)
-                token.level = level - 1
-                token.markup = "linkify"
-                token.info = "auto"
-                nodes.append(token)
+                if lastPos < len(text):
+                    token = Token("text", "", 0)
+                    token.content = text[lastPos:]
+                    token.level = level
+                    nodes.append(token)
 
-                lastPos = links[ln].last_index
-
-            if lastPos < len(text):
-                token = Token("text", "", 0)
-                token.content = text[lastPos:]
-                token.level = level
-                nodes.append(token)
-
-            blockTokens[j].children = tokens = arrayReplaceAt(tokens, i, nodes)
+                blockTokens[j].children = tokens = arrayReplaceAt(tokens, i, nodes)
