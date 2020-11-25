ad369e74685db1cac3a7fe4c8ec8cd515a0967fc
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-11-25 17:59:05 +0200

    🐛 FIX: Exception when document ends in heading

---
+++ tests/test_port/test_no_end_newline.py
@@ -0,0 +1,18 @@
+import pytest
+
+from markdown_it import MarkdownIt
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ("#", "<h1></h1>\n"),
+        ("` `", "<p><code> </code></p>\n"),
+        ("``````", "<pre><code></code></pre>\n"),
+        ("-", "<ul>\n<li></li>\n</ul>\n"),
+    ],
+)
+def test_no_end_newline(input, expected):
+    md = MarkdownIt()
+    text = md.render(input)
+    assert text == expected
