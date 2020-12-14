2eb1fe6b47cc0ad4ebe954cabd91fb8e52a2f03d
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-12-14 20:22:08 +0200

    👌 IMPROVE: Replace `_Entities` with `AttrDict` (#85)

--- markdown_it/common/entities.py
+++ markdown_it/common/entities.py
@@ -1,21 +1,9 @@
 """HTML5 entities map: { name -> characters }."""
 import html.entities
 
+from markdown_it.utils import AttrDict
 
-class _Entities:
-    def __getattr__(self, name):
-        try:
-            return DATA[name]
-        except KeyError:
-            raise AttributeError(name)
-
-    def __getitem__(self, name):
-        return DATA[name]
-
-    def __contains__(self, name):
-        return name in DATA
-
-
-entities = _Entities()
 
 DATA = {name.rstrip(";"): chars for name, chars in html.entities.html5.items()}
+
+entities = AttrDict(DATA)
