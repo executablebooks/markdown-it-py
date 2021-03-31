b41ecf48ff6e0b5a8073038aecf7204db6c8fa10
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-03-31 13:02:43 +0300

    Make markdown_it.common.entities.entities a dict. Remove AttrDict

--- markdown_it/common/entities.py
+++ markdown_it/common/entities.py
@@ -1,9 +1,5 @@
 """HTML5 entities map: { name -> characters }."""
 import html.entities
 
-from markdown_it.utils import AttrDict
 
-
-DATA = {name.rstrip(";"): chars for name, chars in html.entities.html5.items()}
-
-entities = AttrDict(DATA)
+entities = {name.rstrip(";"): chars for name, chars in html.entities.html5.items()}
