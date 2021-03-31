7efdae93e96c2e9838606dc3757b99fff12c67f2
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-03-31 12:23:18 +0300

    Move mypy and flake8 conf to setup.cfg

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# This file is needed for editable installs (`pip install -e .`).
+# Can be removed once the following is resolved
+# https://github.com/pypa/packaging-problems/issues/256
 from setuptools import setup
 
 setup()
