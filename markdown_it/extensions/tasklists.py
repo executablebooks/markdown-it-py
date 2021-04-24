5ea3551a0106abbc5c9d3d3afec578a435865952
Chris Sewell <chrisj_sewell@hotmail.com>
2021-04-24 17:36:35 +0200

    ‼️ BREAKING: Move mdit-py-plugins to install `plugins` extra
    `markdown_it.extensions` was already deprecated,
    and has now been removed, as have all the tests for plugins.
    
    To help check if changes will break plugins,
    a tox env and GH actions job have been added
    that clones the mdit-py-plugins package and runs its test suite.

--- markdown_it/extensions/tasklists.py
+++
@@ -1,9 +0,0 @@
-from mdit_py_plugins.tasklists import *  # noqa: F401,F403
-
-import warnings
-
-warnings.warn(
-    "`markdown_it.extensions` is deprecated, import from `mdit_py_plugins` instead",
-    DeprecationWarning,
-    stacklevel=2,
-)
