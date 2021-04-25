0499c6ce4989d718ffc5bd75dbd261a0255107f5
Chris Sewell <chrisj_sewell@hotmail.com>
2021-04-25 04:43:03 +0200

    ‼️ BREAKING: Move `mdit-py-plugins` install to `plugins` extra (#159)
    `mdit-py-plugins` is no longer a core install requirement.
    `markdown_it.extensions` was already deprecated,
    and has now been removed, as have all the tests for plugins.
    
    To help still check if changes will break plugins,
    a tox env and GH actions job have been added
    that clones the `mdit-py-plugins` package and runs its test suite.

--- markdown_it/extensions/front_matter.py
+++
@@ -1,9 +0,0 @@
-from mdit_py_plugins.front_matter import *  # noqa: F401,F403
-
-import warnings
-
-warnings.warn(
-    "`markdown_it.extensions` is deprecated, import from `mdit_py_plugins` instead",
-    DeprecationWarning,
-    stacklevel=2,
-)
