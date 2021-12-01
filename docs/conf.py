3b560f561e3b20ad5ee35eadfc739a872eaad6e9
Chris Sewell <chrisj_sewell@hotmail.com>
2021-12-01 18:46:04 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+from glob import glob
 import os
 
 # import sys
