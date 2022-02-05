2001ad30fe097497d687cf82b1f4537b867f7ca8
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2022-02-05 11:16:04 +0200

    🔧 MAINTAIN: Add a profiler tox env

---
+++ profiler.py
@@ -0,0 +1,19 @@
+"""A script for profiling.
+
+To generate and read results:
+  - `tox -e profile`
+  - `firefox .tox/prof/output.svg`
+"""
+from pathlib import Path
+
+from markdown_it import MarkdownIt
+
+commonmark_spec = (
+    (Path(__file__).parent / "tests" / "test_cmark_spec" / "spec.md")
+    .read_bytes()
+    .decode()
+)
+
+# Run this a few times to emphasize over imports and other overhead above
+for _ in range(10):
+    MarkdownIt().render(commonmark_spec)
