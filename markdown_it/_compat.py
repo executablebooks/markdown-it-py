8864e54a99aff55ec29dd8dee508189f1360a3dd
Chris Sewell <chrisj_sewell@hotmail.com>
2023-03-25 22:53:06 +0100

    🔧 Minor type annotation fix

--- markdown_it/_compat.py
+++ markdown_it/_compat.py
@@ -4,7 +4,8 @@ from collections.abc import Mapping
 import sys
 from typing import Any
 
+DATACLASS_KWARGS: Mapping[str, Any]
 if sys.version_info >= (3, 10):
-    DATACLASS_KWARGS: Mapping[str, Any] = {"slots": True}
+    DATACLASS_KWARGS = {"slots": True}
 else:
-    DATACLASS_KWARGS: Mapping[str, Any] = {}
+    DATACLASS_KWARGS = {}
