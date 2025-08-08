7de9006a87251169fb6c4be39004f24ae0cca6cf
Chris Sewell <chrisj_sewell@hotmail.com>
2025-08-08 18:41:37 +0200

    ⬆️ Comply with Commonmark 0.31.2
    This PR ports https://github.com/markdown-it/markdown-it/commit/cd2477863fdcc182cc8739e9bedc7363acb344d8,
    which in turn complies with https://spec.commonmark.org/0.31.2/changes.html:
    
    - Unicode:
    
      ```diff
      A [Unicode punctuation character](@) is
      -  an [ASCII punctuation character] or anything in
      - he general Unicode categories  `Pc`, `Pd`, `Pe`, `Pf`, `Pi`, `Po`, or `Ps`.
      + a character in the Unicode `P` (puncuation) or `S` (symbol) general categories.
      ```
    
    - HTML comments:
    
      ```diff
      - An HTML comment consists of `<!--` + *text* + `-->`,
      - where *text* does not start with `>` or `->`, does not end with `-`, and does not contain `--`.
      - (See the [HTML5 spec](http://www.w3.org/TR/html5/syntax.html#comments).)
      + An [HTML comment](@) consists of `<!-->`, `<!--->`, or  `<!--`, a string of characters not including the string `-->`, and `-->`
      + (see the [HTML spec](https://html.spec.whatwg.org/multipage/parsing.html#markup-declaration-open-state)).
      ```
    
    - HTML blocks:
    
      ```diff
      Start condition: line begins the string < or </ followed by one of the strings (case-insensitive)
      - `section`, `source`, `summary`, `table`, `tbody`, `td`,
      + `search`, `section`, `summary`, `table`, `tbody`, `td`,
      ```
    
    - Setext header:
    
      ```diff
      - If a line containing a single `-` can be interpreted as an
      - empty [list items], it should be interpreted this way
      - and not as a [setext heading underline].
      ```

--- tests/test_cmark_spec/get_cmark_spec.py
+++ tests/test_cmark_spec/get_cmark_spec.py
@@ -4,10 +4,12 @@
 # ]
 # ///
 from pathlib import Path
+from typing import Any
 
-default_version = "0.30"
-default_output_path = Path(__file__).parent / "commonmark.json"
-default_fixture_test_path = (
+default_version = "0.31.2"
+default_json_path = Path(__file__).parent / "commonmark.json"
+default_text_path = Path(__file__).parent / "spec.md"
+default_fixture_path = (
     Path(__file__).parent.parent / "test_port" / "fixtures" / "commonmark_spec.md"
 )
 
@@ -23,19 +25,33 @@ def create_argparser():
         help=f"CommonMark spec version to download (default: {default_version})",
     )
     parser.add_argument(
-        "--output",
-        "-o",
+        "--output-json",
         type=Path,
-        default=default_output_path,
-        help=f"Output file path (default: {default_output_path})",
+        default=default_json_path,
+        help=f"Output file path (default: {default_json_path})",
     )
     parser.add_argument(
-        "--test-fixture",
+        "--output-text",
         type=Path,
-        default=default_fixture_test_path,
-        help=f"Write to test fixture (default: {default_fixture_test_path})",
+        default=default_text_path,
+        help=f"Output file path (default: {default_text_path})",
+    )
+    parser.add_argument(
+        "--output-fixture",
+        type=Path,
+        default=default_fixture_path,
+        help=f"Write to test fixture (default: {default_fixture_path})",
     )
     return parser
+
+
+def _json_to_fixture(data: list[dict[str, Any]]) -> str:
+    text = ""
+    for item in data:
+        text += "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+        text += f"src line: {item['start_line'] - 1}\n\n"
+        text += f".\n{item['markdown']}.\n{item['html']}.\n\n"
+    return text
 
 
 if __name__ == "__main__":
@@ -43,35 +59,38 @@ if __name__ == "__main__":
 
     args = create_argparser().parse_args()
     version: str = args.version
-    output_path: Path = args.output
-    write_to_test_fixture = True
-    test_fixture: Path = args.test_fixture
-    changed = False
-    url = f"https://spec.commonmark.org/{version}/spec.json"
-    print(f"Downloading CommonMark spec from {url}")
-    response = requests.get(url)
-    response.raise_for_status()
-    if not output_path.exists() or output_path.read_text() != response.text:
-        changed = True
-        with output_path.open("w") as f:
-            f.write(response.text)
-        print(f"Updated to {output_path}")
-    else:
-        print(f"Spec file {output_path} is up to date, not overwriting")
+    json_path: Path = args.output_json
+    txt_path: Path = args.output_text
+    test_fixture: Path = args.output_fixture
 
-    if write_to_test_fixture:
-        data = response.json()
-        text = ""
-        for item in data:
-            text += "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-            text += f"src line: {item['start_line'] - 1}\n\n"
-            text += f".\n{item['markdown']}.\n{item['html']}.\n\n"
-        if not test_fixture.exists() or test_fixture.read_text() != text:
+    changed = False
+
+    json_url = f"https://spec.commonmark.org/{version}/spec.json"
+    txt_url = f"https://raw.githubusercontent.com/commonmark/commonmark-spec/refs/tags/{version}/spec.txt"
+
+    for url, output_path in ((json_url, json_path), (txt_url, txt_path)):
+        print(f"Downloading CommonMark spec from {url}")
+        response = requests.get(url)
+        response.raise_for_status()
+        if not output_path.exists() or output_path.read_text() != response.text:
             changed = True
-            with test_fixture.open("w") as f:
-                f.write(text)
-            print(f"Also updated to {test_fixture}")
+            with output_path.open("w") as f:
+                f.write(response.text)
+            print(f"Updated to {output_path}")
         else:
-            print(f"Fixture file {test_fixture} is up to date, not overwriting")
+            print(f"File {output_path} is up to date, not overwriting")
+
+    # write_to_test_fixture:
+    response = requests.get(json_url)
+    response.raise_for_status()
+    data = response.json()
+    text = _json_to_fixture(data)
+    if not test_fixture.exists() or test_fixture.read_text() != text:
+        changed = True
+        with test_fixture.open("w") as f:
+            f.write(text)
+        print(f"Also updated to {test_fixture}")
+    else:
+        print(f"Fixture file {test_fixture} is up to date, not overwriting")
 
     raise SystemExit(0 if not changed else 1)
